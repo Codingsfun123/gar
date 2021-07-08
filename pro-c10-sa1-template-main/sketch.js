1ef56ddd4d39dacc7131d0ec8903bee45f7bd39b
Codingsfun123 <86753809+Codingsfun123@users.noreply.github.com>
2021-07-07 21:11:16 -0400

    Add files via upload

---
+++ pro-c10-sa1-template-main/sketch.js
@@ -0,0 +1,23 @@
+var weights = [100, 230, 160, 95];
+var sum = weights[0]+weights[1]+weights[2]+weights[3];
+var average = sum/weights.length;
+console.log(average);
+
+function setup() 
+{
+
+
+
+  createCanvas(400,400);
+
+var sum = weights[0]+weights[1]+weights[2]+weights[3]
+var average = sum/weights.length;
+console.log(average);
+}
+
+function draw() 
+{
+background(51);
+
+}
+
